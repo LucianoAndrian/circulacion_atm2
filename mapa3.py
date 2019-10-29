fed161217efea85356e18e6c335b5a260dec210f
V <44793625+LucianoAndrian@users.noreply.github.com>
2019-10-29 20:44:52 -0300

    mapa 3 con un contour mas

--- mapa3.py
+++ mapa3.py
@@ -14,7 +14,7 @@ import cartopy.crs as ccrs
 #nombre_titulo = ''
 #nombre_archivo = ''
 
-def mapa3(cmin1,cmax1,cmin2,cmax2,ncont,lat,lon,L,VAR1,VAR2,cmap,nombre_titulo,nombre_archivo):
+def mapa3(cmin1,cmax1,cmin2,cmax2,ncont,lat,lon,L,VAR1,VAR2,VAR3,cmap,nombre_titulo,nombre_archivo):
     
     #Pasamos las latitudes/longitudes del dataset a una reticula para graficar
     lons, lats = np.meshgrid(lon, lat)
@@ -38,10 +38,11 @@ def mapa3(cmin1,cmax1,cmin2,cmax2,ncont,lat,lon,L,VAR1,VAR2,cmap,nombre_titulo,n
     #Graficamos
     im1=ax.contourf(lons, lats, VAR1/100000, clevs1, cmap=plt.get_cmap(cmap), extend='both', transform=crs_latlon)
     ax.contour(lons, lats, VAR1/100000, levels = 0, colors = "w", xtend='both', transform=crs_latlon)
+
   
     im2=bx.contourf(lons, lats, VAR2/100000, clevs2, cmap=plt.get_cmap(cmap), extend='both', transform=crs_latlon)
     bx.contour(lons, lats, VAR2/100000, levels = 0, colors = "w", xtend='both', transform=crs_latlon)
-    
+
     
     # Defino los ejes donde van a estar las colorbar
     cbaxes1 = fig.add_axes([0.82, 0.55, 0.02, 0.4]) 
@@ -55,8 +56,17 @@ def mapa3(cmin1,cmax1,cmin2,cmax2,ncont,lat,lon,L,VAR1,VAR2,cmap,nombre_titulo,n
     
     cb2 = plt.colorbar(im2, fraction=0.052, pad=0.04, shrink=0.7, aspect=9,  cax = cbaxes2)   #TIENEN QUE ESTAR EN DIF LUGARES
     cb2.ax.tick_params(labelsize=5.8)
-    cb2.set_label('$x10^{5}$', labelpad = -30, y=1.08, rotation=0,fontsize=5.8)
+    cb2.set_label('$x10^{9}$', labelpad = -30, y=1.08, rotation=0,fontsize=5.8)
     
+   
+    # agrega las lineas de VAR3. (van despues de las barras de colores, para que tengan color aparte)
+    
+    anom = ax.contour(lons , lats, VAR3, transform=crs_latlon, linewidths=0.5, colors = "black")
+    ax.clabel(anom, inline=1, fontsize = 4)
+    
+    anom = bx.contour(lons , lats, VAR3, transform=crs_latlon, linewidths=0.5, colors = "black")
+    bx.clabel(anom, inline=1, fontsize = 4)
+ 
 
     #Características del mapa
     ax.add_feature(cartopy.feature.LAND, facecolor='#d9d9d9')
@@ -73,6 +83,8 @@ def mapa3(cmin1,cmax1,cmin2,cmax2,ncont,lat,lon,L,VAR1,VAR2,cmap,nombre_titulo,n
     ax.tick_params(labelsize=6)
     plt.tight_layout()
     
+
+    
     
     bx.add_feature(cartopy.feature.LAND, facecolor='#d9d9d9')
     bx.add_feature(cartopy.feature.COASTLINE)
@@ -87,10 +99,9 @@ def mapa3(cmin1,cmax1,cmin2,cmax2,ncont,lat,lon,L,VAR1,VAR2,cmap,nombre_titulo,n
     bx.yaxis.set_major_formatter(lat_formatter)
     bx.tick_params(labelsize=6)
     plt.tight_layout()
-
     
     #Titulo
-    #plt.title(nombre_titulo, fontsize=6, y=2.2, loc="center")
-    
+    #plt.title(nombre_titulo, fontsize=6, y=2.2, x = -17) # con esto el titulo queda centrado 
+                                                          # pero al guardar lo corta mal al titulo
     #Guardar figura
     plt.savefig(nombre_archivo + '.jpg')
