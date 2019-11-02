32cd2b01f2cdacaddc75040f3fb2c08674dd40b9
V <44793625+LucianoAndrian@users.noreply.github.com>
2019-11-02 19:51:16 -0300

    FINAL

--- P3.py
+++ P3.py
@@ -6,14 +6,8 @@ as
 @author:   auri
 """
 import numpy as np
-import time
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-import math
-import time 
 import os
-
-#os.chdir('/Users/mini/Documents/Circulación/Atmósfera/P3/')  #Esto es para que se quede en el directorio que querés
 
 import xarray as xr
 from netCDF4 import Dataset 
@@ -21,22 +15,23 @@ import cartopy.feature
 from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
 import cartopy.crs as ccrs
 
-from DerY import derivy # LA ULTIMA QUE SUBIO LEAN
+from DerY import derivy 
 from DerX import derivx     
 from numpy import empty
-
 
 # nuestras funciones
 from Estado_basico import Estado_basico
 from mapa import mapa
 from mapa2 import mapa2
-from mapa3 import mapa3 ## CON CAMBIOS
-from mapa4 import mapa4 ##NUEVA
-from mapa5 import mapa5 ##NUEVA
+from mapa3 import mapa3 ## con cambios
+from mapa4 import mapa4 
+from mapa5 import mapa5 
 
 
 dir = '/home/auri/Facultad/Materias/Circulacion/TP6/' # Luchi
 script_dir = os.path.dirname(dir)
+
+#os.chdir('/Users/mini/Documents/Circulación/Atmósfera/P3/')  #Esto es para que se quede en el directorio que querés
 #dir = '/Users/mini/Documents/Circulación/Atmósfera/P3/' # Mili
 
 #%%
@@ -44,7 +39,7 @@ script_dir = os.path.dirname(dir)
     ### EB1P1 ###
     #############
     
-dS = xr.open_dataset(dir+'EB1P1_concatenado.nc', decode_times=False) #Abro el NetCdf
+dS = xr.open_dataset(dir+'EB1P1_concatenado.nc', decode_times=False) 
 print(dS)
 
 lat=dS['lat'].values
@@ -53,12 +48,11 @@ u = dS["ucomp"].values
 v = dS["vcomp"].values
 h = dS["h"].values
 
-H = 40000 #h0 del name list
-g = 9.8 #gravedad
-rho = 1 #densidad del aire
+H = 40000 # h0 del name list
+g = 9.8 # gravedad
+rho = 1 # densidad del aire
 
 eta = (h - H)/9.8  #superficie libre 
-
 
 # Calculamos el estado basico de las velocidades y la superficie libre
 
@@ -66,54 +60,47 @@ u_b = Estado_basico(u, lat, lon)
 v_b = Estado_basico(v, lat, lon)
 eta_b = Estado_basico(eta, lat, lon)
 
-#Calculamos las perturbaciones
+# Calculamos las perturbaciones
+
 u_e = u - u_b           
 v_e = v - v_b           
 eta_e = eta - eta_b   
 
-
-psi = dS['stream'].values  
-
-psi_b = Estado_basico(psi, lat, lon)
-
-
-
-psi_e = psi - psi_b
-
-
-
 #%%
 # Energia cinetica
+
 Ec = (rho*H/g)/2*(u**2 + v**2)
 Ec_b = (rho*H/g)/2*(u_b**2 + v_b**2)
 Ec_e = (rho*H/g)/2*(u_e**2 + v_e**2)
 
 
 # Energia potencial
+
 Ep = (rho*g)/2*(eta**2+(H/g)**2) # Energia potencial instantanea
 Ep_b = (rho*g)/2*(eta_b**2+(H/g)**2) # Energia potencial del flujo medio
 Ep_e = (rho*g)/2*(eta_e**2) # Energia potencial de las perturbaciones
 
 
 # Graficos Ec y Ep del flujo medio
-#Defino parámetros para poder graficar
+# Defino parámetros para poder graficar
+
 LONMIN= 0
 LONMAX= 359
 LATMIN= -88
 LATMAX= 88
 L = [LONMIN, LONMAX, LATMIN, LATMAX]
-cmap = 'YlOrRd'
+cmap = 'YlOrRd'  # cambiando los colores para estos mapas
 
-VAR1 = Ec_b
-VAR2 = Ep_b/10000
+VAR1 = Ec_b/1e4   # dividir por 1e4 en todas las energia. asi quedan bien las escalas de la funcion mapa3
+VAR2 = Ep_b/1e4
 VAR3 = eta_e[51,:,:] #anomalia de la sfc libre dia 2 de la perturbaciòn
 
 cmin1 = 0   #Límites de la E cinética
-cmax1 = 25
+cmax1 = 24
 ncont = 25
 
-cmax2 =  854265.94 #Límites de E potencial 
-cmin2 =  854264.94  # esto es un mierda jajaja --> la escala cambio a 1e9 porque se habian borrado los ** en el calculo de la energia
+cmax2 = 8250 #Límites de E potencial 
+cmin2 = 8180   
 
 nombre_titulo1 = "Energía Cinética - Estado básico 1"
 nombre_titulo2 = "Energía Potencial - Estado básico 1"
@@ -124,18 +111,17 @@ fig= mapa3(cmin1,cmax1,cmin2,cmax2,ncont,lat,lon,L,VAR1,VAR2,VAR3,cmap,nombre_ti
 
 # Graficos Ec y Ep de las perturbaciones dia 2 de la perturbaciòn
 
-VAR1 = Ec_e[51,:,:]
-VAR2 = Ep_e[51,:,:]
+VAR1 = Ec_e[51,:,:]/1e4
+VAR2 = Ep_e[51,:,:]/1e4
 VAR3 = eta_e[51,:,:]
 
-cmap = 'YlOrRd' # cambiando los colores porque queda horrible sino
  
 cmin1 = 0
 cmax1 = 3.5
 ncont = 25
 
 cmin2 = 0
-cmax2 = 0.04
+cmax2 = 0.25
 
 nombre_titulo1 = "Anomalía Energìa Cinética - Estado básico 1"
 nombre_titulo2 = "Anomalía Energìa Potencial - Estado básico 1"
@@ -144,7 +130,8 @@ fig= mapa3(cmin1,cmax1,cmin2,cmax2,ncont,lat,lon,L,VAR1,VAR2,VAR3,cmap,nombre_ti
 #%%
 
 # para el ej2
-# derivx requiere una escala en x que depende de la latitud 
+# derivx requiere una escala en x que depende de la latitud
+ 
 R = 6370000
 dx = (R*2*np.pi)/256
 dy = (R*np.pi)/128
@@ -162,7 +149,7 @@ u_b_y = derivy(u_b, dy)
 v_b_x = derivx(v_b, dx, lat)
 v_b_y = derivy(v_b, dy)
 
-C_barotropica = -rho*H*(u_e**2*u_b_x + u_e*v_e*u_b_y + u_e*v_e*v_b_x + v_e**2*v_b_y)
+C_barotropica = -rho*H/g*(u_e**2*u_b_x + u_e*v_e*u_b_y + u_e*v_e*v_b_x + v_e**2*v_b_y)
 
 
 # conversion baroclinica
@@ -188,7 +175,7 @@ lon_m, lat_m = np.meshgrid(lon, lat)
 lat_g_m = lat_m*np.pi/180 # regla de tres para pasar de grados a radianes.
 
 
-# viento geostrofico
+# viento ageostrofico
 
 Ug = -1/(2*omega*np.sin(lat_g_m))*derivy(h, dy)
 Vg = 1/(2*omega*np.sin(lat_g_m))*derivx(h, dx, lat)
@@ -202,16 +189,7 @@ Vg_e = Vg - Vg_b
 Uag = u_e - Ug_e
 Vag = v_e - Vg_e
 
-#U_ag = u - Ugx 
-#V_ag = v - Vgy
 
-# viento agesotrofico
-
-#V_ag_b = Estado_basico(V_ag, lat, lon)
-#U_ag_b = Estado_basico(U_ag, lat, lon)
-
-#V_ag_anom = V_ag - V_ag_b
-#U_ag_anom = U_ag - U_ag_b
 
 #%%
 ## GRAFICOS ##
@@ -227,14 +205,15 @@ LATMIN= -60
 LATMAX= 5
 L = [LONMIN, LONMAX, LATMIN, LATMAX]
 
-cmin = -10
-cmax = 10
+cmin = -5
+cmax = 5
 cmap = 'Spectral_r'
 dia = ("1", "2", "3", "4")
 
-p=86400/1e4
+p=86400/1e4 # para que sea por dia y que la escala quede razonable
+
 for i in np.arange(0,4,1):
-    VAR1 = Adv[50+i, 20:67, 128:214]*p# - Adv[50+i-1, 20:67, 128:214] # asi es la anomalia con respecto al dia anterior (asi lo piden las unidades)
+    VAR1 = Adv[50+i, 20:67, 128:214]*p
     VAR2 = Ec_e[50+i, 20:67, 128:214]/10000 
 
     nombre_titulo = "Advección de Ke y Ke - Dia " + dia[i] + " Estado básico 1"
@@ -245,13 +224,11 @@ for i in np.arange(0,4,1):
 
 # c baroclinica
 
-#cmin = -2
-#cmax = 2
 cmap = 'Spectral_r'
 dia = ("1", "2", "3", "4")
 
 for i in np.arange(0,4,1):
-    VAR1 = C_baroclinica[50+i, 20:67, 128:214]*p#*86400/1e4# - C_baroclinica[50+i-1, 20:67, 128:214]
+    VAR1 = C_baroclinica[50+i, 20:67, 128:214]*p
     VAR2 = Ec_e[50+i, 20:67, 128:214]/10000
 
     nombre_titulo = "Conversión baroclínica y Ke - Dia " + dia[i] + " Estado básico 1"
@@ -262,17 +239,15 @@ for i in np.arange(0,4,1):
     
 # Dispersion y flujo agesotrofico
 
-lat3=lat[20:70]   # NUEVAS LATITUDES PARA QUE GRAFIQUE LOS VECTORES CERCA DEL ECUADOR
+lat3=lat[20:70]   # LATITUDES PARA QUE GRAFIQUE LOS VECTORES CERCA DEL ECUADOR
 lon3=lon[128:214]
 lat4=lat[20:56]
 
-#cmin = -60
-#cmax = 60
 cmap = 'Spectral_r'
 dia = ("1", "2", "3", "4")
-psi_e[50+i, 20:70, 128:214]
+
 for i in np.arange(0,4,1):
-    VAR1 = Disp[50+i, 20:70, 128:214]*p# - Disp[50+i-1, 20:70, 128:214]
+    VAR1 = Disp[50+i, 20:70, 128:214]*p
     VAR2 = Ec_e[50+i, 20:70, 128:214]/10000
 
     
@@ -284,15 +259,14 @@ for i in np.arange(0,4,1):
     
     mapa5(cmin,cmax,ncont,lat3,lat4,lon3,L,VAR1,VAR2,U,V,cmap,nombre_titulo,nombre_archivo)
     
+    
 # c barotropica
     
-#cmin = -1
-#cmax = 1
 cmap = 'Spectral_r'
 dia = ("1", "2", "3", "4")
 
 for i in np.arange(0,4,1):
-    VAR1 = C_barotropica[50+i, 20:67, 128:214]*p#-C_barotropica[50+i-1, 20:67, 128:214]
+    VAR1 = C_barotropica[50+i, 20:67, 128:214]*p
     VAR2 = Ec_e[50+i, 20:67, 128:214]/10000
 
     nombre_titulo = "Conversión barotrópica y Ke - Dia " + dia[i] + " Estado básico 1"
