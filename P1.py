25e4c57d23b69fdb8fbbbfce0c512922c13ea4ad
V <44793625+LucianoAndrian@users.noreply.github.com>
2019-11-06 20:15:31 -0300

    informe
    mili: la P1.py que tenias era del drive. creo q nunca la actualice. en esta que subo esta todo lo de la p1.
    cambie escalas y colores. cambia lo que quieras. usa el ultimo commit de mapa.py despues la podemos modificar para cambiar el label de la barra para cada grafico q es lo q faltaria

--- P1.py
+++ P1.py
@@ -5,7 +5,8 @@ import matplotlib.animation as animation
 import math
 import time 
 import os
-os.chdir('/Users/mini/Documents/Circulación/Atmósfera')
+# os.chdir('/Users/mini/Documents/Circulación/Atmósfera')
+os.chdir('/home/auri/Facultad/Materias/Circulacion/TP4/')
 import xarray as xr
 from netCDF4 import Dataset 
 import cartopy.feature
@@ -14,12 +15,13 @@ import cartopy.crs as ccrs
 from mapa import mapa
 from DerY import derivy 
 #%%
-dir = '/Users/mini/Documents/Circulación/Atmósfera/Experimento1/shallow.nc'
+# dir = '/Users/mini/Documents/Circulación/Atmósfera/Experimento1/shallow.nc' # Mili
 
+dir = '/home/auri/Facultad/Materias/Circulacion/TP4/Experimento1/shallow.nc' # Luchi
 dS = xr.open_dataset(dir, decode_times=False) #Abro el NetCdf
 print(dS)       # visualizo la info del .nc
 
-#Las variables están dimensionalizadas
+# Las variables están dimensionalizadas
 forzante = dS['fr'].values
 u=dS['ucomp'].values
 v=dS['vcomp'].values
@@ -30,22 +32,20 @@ vort=dS['vor'].values
 lat=dS['lat'].values
 lon=dS['lon'].values
 
-beta=2*10**-11
-
 #%% Grafico la velocidad zonal en el último paso temporal (el 50)
 
-VAR=u[49,:,:]
+VAR = u[49,:,:]
 cmin = 0
-cmax = 10
+cmax = 15
 ncont = 11
 clevs = np.linspace(cmin, cmax, ncont)
-LONMIN=0
-LONMAX=359
-LATMIN=-88
-LATMAX=88
+LONMIN = 0
+LONMAX = 359
+LATMIN = -88
+LATMAX = 88
 L = [LONMIN, LONMAX, LATMIN, LATMAX]
-cmap = 'rainbow'
-nombre_titulo = 'u'
+cmap = 'YlOrRd'
+nombre_titulo = 'Componente Zonal del viento Simulacion 1'
 nombre_archivo = 'u_tiempo_50_EB1'
 
 
@@ -54,42 +54,298 @@ fig = mapa(cmin,cmax,ncont,lat,lon,L,VAR,cmap,nombre_titulo,nombre_archivo)
 #%%
 #Grafico forzante en el "estado estacionario"
 
-VAR=forzante[49,:,:]
+VAR = forzante[49,:,:]
 cmin = 40000
-cmax = 50000
+cmax = 60000 # un poco por arriba del maximo real para que el maximo del EB2 no quede tan bajo
 ncont = 11
 clevs = np.linspace(cmin, cmax, ncont)
-LONMIN=0
-LONMAX=359
-LATMIN=-88
-LATMAX=88
+LONMIN = 0
+LONMAX = 359
+LATMIN = -88
+LATMAX = 88
 L = [LONMIN, LONMAX, LATMIN, LATMAX]
-cmap = 'rainbow'
-nombre_titulo = 'Forzante'
-nombre_archivo = 'forzante_tiempo_50_EB1'
+cmap = 'YlOrRd'
+nombre_titulo = 'Forzante simulacion 1'
+nombre_archivo = 'forzante_tiempo_50_EA1'
 
 
 fig = mapa(cmin,cmax,ncont,lat,lon,L,VAR,cmap,nombre_titulo,nombre_archivo)
 
-#%% Graficamos la vorticidad absoluta
-R=6370000
-omega=7.27*10**-5
-beta=2*omega*np.cos(lat)/R
-vort_abs=vort+beta
+#%% Graficar el gradiente meridional de vorticidad absoluta
 
-VAR=vort[49,:,:] #es la vorticidad relativa
-cmin = -5*10**-6
-cmax = 5*10**-6
+# gradiente vorticidad planetaria
+R = 6370000
+omega = 7.27*10**-5
+
+# gradiente de vorticidad relativa
+
+# longitud de arco en radianes es R*angulo--> 6370000*pi
+# para tener el dy--> longitud de arco/128 (puntos de grilla)
+
+lat_g = lat*np.pi/180 # regla de tres para pasar de grados a radianes.
+beta = omega*np.cos(lat_g)/R
+long_arco = R*lat_g
+
+dy = R*np.pi/128
+
+vort_y = derivy(vort[49,:,:], dy)
+
+# haciendo lo mismo que antes pero de forma matricial simplifica los proximos pasos
+
+lon_m, lat_m = np.meshgrid(lon, lat) 
+lat_g_m = lat_m*np.pi/180 # regla de tres para pasar de grados a radianes.
+beta_m = omega*np.cos(lat_g_m)/R
+long_arco_m = R*lat_g
+
+vort_abs = vort_y + beta_m
+
+# gradiente de vorticidad relativa #ESTO CREO Q NO VA AL INFORME
+#VAR= vort_y 
+#cmin = np.round(np.min(vort_y), 14) # redondea con 15 digitos..ya que es 10E-13
+#cmax = np.round(np.max(vort_y), 14) # este con 13 porque es 10E-11
+#ncont = 11
+#clevs = np.linspace(cmin, cmax, ncont)
+#LONMIN = 0
+#LONMAX = 359
+#LATMIN = -88
+#LATMAX = 88
+#L = [LONMIN, LONMAX, LATMIN, LATMAX]
+#cmap = 'bwr'
+#nombre_titulo = 'Gradiente meridional de vorticidad relativa simulacion 1'
+#nombre_archivo = 'vorticidad_gradiente_relativa_EC1'
+
+
+#fig = mapa(cmin,cmax,ncont,lat,lon,L,VAR,cmap,nombre_titulo,nombre_archivo)
+
+# gradiente de vort planetaria  # ESTO TAMPOCO
+#VAR= beta_m
+#cmin = np.round(np.min(beta_m), 15) # redondea con 15 digitos..ya que es 10E-13
+#cmax = np.round(np.max(beta_m), 12) # este con 13 porque es 10E-11
+#ncont = 11
+#clevs = np.linspace(cmin, cmax, ncont)
+#LONMIN = 0
+#LONMAX = 359
+#LATMIN = -88
+#LATMAX = 88
+#L = [LONMIN, LONMAX, LATMIN, LATMAX]
+#cmap = 'YlOrRd'
+#nombre_titulo = 'Gradiente meridional de vorticidad planetaria simulacion 1'
+#nombre_archivo = 'vorticidad_gradiente_planetaria_EC1'
+
+
+#fig = mapa(cmin,cmax,ncont,lat,lon,L,VAR,cmap,nombre_titulo,nombre_archivo)
+
+
+VAR= vort_abs #es la vorticidad relativa
+cmin = 0#np.round(np.min(vort_abs), 15) # redondea con 15 digitos..ya que es 10E-13
+cmax = 1.5e-11#np.round(np.max(vort_abs), 13) # este con 13 porque es 10E-11
 ncont = 11
 clevs = np.linspace(cmin, cmax, ncont)
-LONMIN=0
-LONMAX=359
-LATMIN=-88
-LATMAX=88
+LONMIN = 0
+LONMAX = 359
+LATMIN = -88
+LATMAX = 88
 L = [LONMIN, LONMAX, LATMIN, LATMAX]
-cmap = 'rainbow'
-nombre_titulo = 'Vorticidad'
-nombre_archivo = 'vorticidad_tiempo_50_EB1'
+cmap = 'YlOrRd'
+nombre_titulo = 'Gradiente meridional de vorticidad absoluta simulacion 1'
+nombre_archivo = 'vorticidad_gradiente_EC1'
+
+fig = mapa(cmin,cmax,ncont,lat,lon,L,VAR,cmap,nombre_titulo,nombre_archivo)
+
+#%% KS 
+# necesitamos la derivada 2da de U respecto de y
+uy = derivy(u[49,:,:],dy)
+uyy = derivy(uy,dy)
+
+ks = np.sqrt((beta_m-uyy)/u[49,:,:])
+
+# por circulo de latitud. KS tiene los mismo valores para cada longitud, solo varia latitud
+ks_c = R*np.cos(lat_g_m)*ks
+
+VAR = ks_c #es la vorticidad relativa
+cmin = np.round(np.min(ks_c)) # redondea con 15 digitos..ya que es 10E-13
+cmax = 20#np.round(np.max(ks_c)) # este con 13 porque es 10E-11
+ncont = 11
+clevs = np.linspace(cmin, cmax, ncont)
+LONMIN= 0
+LONMAX= 359
+LATMIN= -88
+LATMAX= 88
+L = [LONMIN, LONMAX, LATMIN, LATMAX]
+cmap = 'YlOrRd'
+nombre_titulo = 'KS por circulo de latitud Simulacion 1'
+nombre_archivo = 'KS_ED1'
 
 
 fig = mapa(cmin,cmax,ncont,lat,lon,L,VAR,cmap,nombre_titulo,nombre_archivo)
+
+
+
+#%%  SIMULACION 2
+# dir = '/Users/mini/Documents/Circulación/Atmósfera/Simulacion2/shallow.nc' # Mili
+dir = '/home/auri/Facultad/Materias/Circulacion/TP4/Simulacion2/shallow.nc' # Luchi
+dS = xr.open_dataset(dir, decode_times=False) #Abro el NetCdf
+print(dS)       # visualizo la info del .nc
+
+# Las variables están dimensionalizadas
+forzante = dS['fr'].values
+u=dS['ucomp'].values
+v=dS['vcomp'].values
+forzante = dS['fr'].values
+vort=dS['vor'].values
+
+
+lat=dS['lat'].values
+lon=dS['lon'].values
+
+
+
+#%% Grafico la velocidad zonal en el último paso temporal (el 50)
+
+VAR = u[49,:,:]
+cmin = -15
+cmax = 15
+ncont = 11
+clevs = np.linspace(cmin, cmax, ncont)
+LONMIN = 0
+LONMAX = 359
+LATMIN = -88
+LATMAX = 88
+L = [LONMIN, LONMAX, LATMIN, LATMAX]
+cmap = 'RdYlBu_r'
+nombre_titulo = 'Componente Zonal del viento Simulacion 2'
+nombre_archivo = 'u_tiempo_50_EB2'
+
+
+fig = mapa(cmin,cmax,ncont,lat,lon,L,VAR,cmap,nombre_titulo,nombre_archivo)
+
+#%%
+#Grafico forzante en el "estado estacionario"
+
+VAR=forzante[49,:,:]
+cmin = np.round(np.min(forzante[49,:,:]))
+cmax = 60000#np.round(np.max(forzante[49,:,:])) # para q este a la misma escala que el EB1 y que se note la itcz
+clevs = np.linspace(cmin, cmax, ncont)
+LONMIN= 0
+LONMAX= 359
+LATMIN= -88
+LATMAX= 88
+L = [LONMIN, LONMAX, LATMIN, LATMAX]
+cmap = 'YlOrRd'
+nombre_titulo = 'Forzante simulacion 2'
+nombre_archivo = 'forzante_tiempo_50_EA2'
+
+
+fig = mapa(cmin,cmax,ncont,lat,lon,L,VAR,cmap,nombre_titulo,nombre_archivo)
+
+#%% Graficar el gradiente meridional de vorticidad absoluta
+
+# gradiente vorticidad planetaria
+R = 6370000
+omega = 7.27*10**-5
+
+# gradiente de vorticidad relativa
+
+# longitud de arco en radianes es R*angulo--> 6370000*pi
+# para tener el dy--> longitud de arco/128 (puntos de grilla)
+
+lat_g = lat*np.pi/180 # regla de tres para pasar de grados a radianes.
+beta = omega*np.cos(lat_g)/R
+long_arco = R*lat_g
+
+dy = R*np.pi/128
+
+vort_y = derivy(vort[49,:,:], dy)
+
+# haciendo lo mismo que antes pero de forma matricial simplifica los proximos pasos
+
+lon_m, lat_m = np.meshgrid(lon, lat) 
+lat_g_m = lat_m*np.pi/180 # regla de tres para pasar de grados a radianes.
+beta_m = omega*np.cos(lat_g_m)/R
+long_arco_m = R*lat_g
+
+vort_abs = vort_y + beta_m
+
+
+
+# gradiente de vorticidad relativa
+#VAR= vort_y 
+#cmin = np.round(np.min(vort_y), 14) # redondea con 15 digitos..ya que es 10E-13
+#cmax = np.round(np.max(vort_y), 14) # este con 13 porque es 10E-11
+#ncont = 11
+#clevs = np.linspace(cmin, cmax, ncont)
+#LONMIN = 0
+#LONMAX = 359
+#LATMIN = -88
+#LATMAX = 88
+#L = [LONMIN, LONMAX, LATMIN, LATMAX]
+#cmap = 'winter'
+#nombre_titulo = 'Gradiente meridional de vorticidad relativa simulacion 2'
+#nombre_archivo = 'vorticidad_gradiente_relativa_EC2'
+
+
+#fig = mapa(cmin,cmax,ncont,lat,lon,L,VAR,cmap,nombre_titulo,nombre_archivo)
+
+# gradiente de vort planetaria
+#VAR= beta_m
+#cmin = np.round(np.min(beta_m), 15) # redondea con 15 digitos..ya que es 10E-13
+#cmax = np.round(np.max(beta_m), 12) # este con 13 porque es 10E-11
+#ncont = 11
+#clevs = np.linspace(cmin, cmax, ncont)
+#LONMIN = 0
+#LONMAX = 359
+#LATMIN = -88
+#LATMAX = 88
+#L = [LONMIN, LONMAX, LATMIN, LATMAX]
+#cmap = 'winter'
+#nombre_titulo = 'Gradiente meridional de vorticidad planetaria simulacion 2'
+#nombre_archivo = 'vorticidad_gradiente_planetaria_EC2'
+
+
+#fig = mapa(cmin,cmax,ncont,lat,lon,L,VAR,cmap,nombre_titulo,nombre_archivo)
+
+
+VAR= vort_abs 
+cmin = -1.5e-11 # redondea con 14 digitos..ya que es 10E-12
+cmax = 1.5e-11 # este con 13 porque es 10E-11
+ncont = 11
+clevs = np.linspace(cmin, cmax, ncont)
+LONMIN = 0
+LONMAX = 359
+LATMIN = -88
+LATMAX = 88
+L = [LONMIN, LONMAX, LATMIN, LATMAX]
+cmap = 'RdYlBu_r'
+nombre_titulo = 'Gradiente meridional de vorticidad absoluta simulacion 2'
+nombre_archivo = 'vorticidad_gradiente_EC2'
+
+
+fig = mapa(cmin,cmax,ncont,lat,lon,L,VAR,cmap,nombre_titulo,nombre_archivo)
+
+#%% KS 
+# necesitamos la derivada 2da de U respecto de y
+uy = derivy(u[49,:,:],dy)
+uyy = derivy(uy,dy)
+
+ks = np.sqrt((beta_m-uyy)/u[49,:,:])
+
+# por circulo de latitud. KS tiene los mismo valores para cada longitud, solo varia latitud
+ks_c = R*np.cos(lat_g_m)*ks
+
+VAR = ks_c #es la vorticidad relativa
+cmin = np.round(np.nanmin(ks_c))
+cmax = 20 #np.round(np.nanmax(ks_c)) #maximo igual al EB1
+ncont = 11
+clevs = np.linspace(cmin, cmax, ncont)
+LONMIN= 0
+LONMAX= 359
+LATMIN= -88
+LATMAX= 88
+L = [LONMIN, LONMAX, LATMIN, LATMAX]
+cmap = 'YlOrRd'
+nombre_titulo = 'KS por circulo de latitud Simulacion 2'
+nombre_archivo = 'KS_ED2'
+
+
+fig = mapa(cmin,cmax,ncont,lat,lon,L,VAR,cmap,nombre_titulo,nombre_archivo)
+
--- mapa.py
+++ mapa.py
@@ -10,6 +10,7 @@ from matplotlib import pyplot as plt
 import cartopy.feature
 from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
 import cartopy.crs as ccrs
+import os
 
 #L = [LONMIN, LONMAX, LATMIN, LATMAX]
 #cmap = 'rainbow'
@@ -17,6 +18,9 @@ import cartopy.crs as ccrs
 #nombre_archivo = 'forzante_tiempo_50_EB1'
 
 def mapa(cmin,cmax,ncont,lat,lon,L,VAR,cmap,nombre_titulo,nombre_archivo):
+    #dir = '/home/auri/Facultad/Materias/Circulacion/TP5/Simulaciones/' # Luchi
+    #script_dir = os.path.dirname(dir)
+    #results_dir = os.path.join(script_dir, 'salidas/')  
     
     #Pasamos las latitudes/longitudes del dataset a una reticula para graficar
     lons, lats = np.meshgrid(lon, lat)
@@ -24,7 +28,7 @@ def mapa(cmin,cmax,ncont,lat,lon,L,VAR,cmap,nombre_titulo,nombre_archivo):
     clevs = np.linspace(cmin, cmax, ncont)
     
     #Creamos figura
-    fig=plt.figure(figsize=(4,3),dpi=200)
+    fig=plt.figure(figsize=(6,4),dpi=200)
     
     #Definimos proyección
     ax = plt.axes(projection=ccrs.PlateCarree(central_longitude=180))
@@ -32,12 +36,12 @@ def mapa(cmin,cmax,ncont,lat,lon,L,VAR,cmap,nombre_titulo,nombre_archivo):
     ax.set_extent(L, crs=crs_latlon)
     
     #Graficamos
-    im=ax.contourf(lons, lats, VAR/100000, clevs, cmap=plt.get_cmap(cmap), extend='both', transform=crs_latlon)
-    ax.contour(lons, lats, VAR/100000, levels = 0, colors = "w", xtend='both', transform=crs_latlon)
+    im=ax.contourf(lons, lats, VAR, clevs, cmap=plt.get_cmap(cmap), extend='both', transform=crs_latlon)
+    ax.contour(lons, lats, VAR, levels = 0, colors = "w", xtend='both', transform=crs_latlon)
     #Agregamos barra de colores
     cb = plt.colorbar(im, fraction=0.052, pad=0.04, shrink=0.8, aspect=8)
     cb.ax.tick_params(labelsize=6)
-    cb.set_label('$x10^{5}$', labelpad = -30, y=1.12, rotation=0,fontsize=6)
+    cb.set_label("",labelpad = -29, y=1.14, rotation=0,fontsize=6)
     #Características del mapa
     ax.add_feature(cartopy.feature.LAND, facecolor='#d9d9d9')
     ax.add_feature(cartopy.feature.COASTLINE)
@@ -59,3 +63,4 @@ def mapa(cmin,cmax,ncont,lat,lon,L,VAR,cmap,nombre_titulo,nombre_archivo):
     
     #Guardar figura
     plt.savefig(nombre_archivo + '.jpg')
+    plt.close()
